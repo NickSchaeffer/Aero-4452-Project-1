975228539566fffdc1e7de7000d9728665afdb70
NickSchaeffer <158086487+NickSchaeffer@users.noreply.github.com>
2026-09-15 13:17:44 -0700

    Update Target_Prop.py

--- Target_Prop.py
+++ Target_Prop.py
@@ -7,6 +7,7 @@ inc, RAAN, ecc, omega, theta, n_rev, date = tle2coes("SDO_TLE.txt")
 n = n_rev * 2 * np.pi / 86400 
 a = (mu / n**2) ** (1/3)
 h = np.sqrt(mu*a*(1-ecc**2))
+ecc = 0 
 
 R,V = coes2RV(h,ecc,RAAN,inc,omega,theta,mu)
 
