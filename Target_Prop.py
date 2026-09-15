e73f9f8bba740d6dfc67a2646dc40c415c09c58c
NickSchaeffer <158086487+NickSchaeffer@users.noreply.github.com>
2026-09-15 11:09:15 -0700

    pulled coes from tle and got initial r and v vects

--- Target_Prop.py
+++ Target_Prop.py
@@ -1,7 +1,12 @@
 import numpy as np
 from Project_Functions import coes2RV
+from Project_Functions import tle2coes
+mu = 398600
+inc, RAAN, ecc, omega, theta, n_rev, date = tle2coes("SDO_TLE.txt")
 
+n = n_rev * 2 * np.pi / 86400 
+a = (mu / n**2) ** (1/3)
+h = np.sqrt(mu*a*(1-ecc**2))
 
-
-R,V = coes2RV("SDO_TLE.txt")
+R,V = coes2RV(h,ecc,RAAN,inc,omega,theta,mu)
 
